27bcc51e31560fa3dc57e67e516b8c16515c44e6
programmer9wc <programmer9@webbycrown.com>
2025-02-22 11:17:56 +0530

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # query_builder
-query-builder
+# test-extension
+
+## 1. Installation:
+
+- Install the package below.
+```
+composer require programmer9wc/query_builder:dev-main
+```
+
---
+++ src/Providers/QueryBuilderServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Programmer9WC\QueryBuilder\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Route;
+
+class QueryBuilderServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot(Router $router)
+    {
+        //
+    }
+    
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+    
+
+}
